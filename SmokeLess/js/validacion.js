ad20c6a663b7ade788902b384b5bb7793e6c7442
azurim04 <alopezc1904@alumno.ipn.mx>
2021-05-03 00:44:44 -0500

    validacion

---
+++ SmokeLess/js/validacion.js
@@ -0,0 +1,37 @@
+function validar(formulario){
+    var cadenas = formulario.getElementsByClassName("cadena")
+    var numeros = formulario.getElementsByClassName("numero")
+    var email = formulario.getElementsByClassName("email")
+    var esEmail = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/
+    var esNumero = /^[0-9]+$/
+    var esCadena = /([A-Za-z])+\D?/
+    for(var i=0;i<cadenas.length;i++){
+        if (!esCadena.test(cadenas[i].value)){
+            alert("recuerda ingresar solo letras y no dejar el espacio vacio")
+            return false;
+        }
+    }
+    for(var i=0;i<numeros.length;i++){
+        if (!esNumero.test(numeros[i].value)){
+            alert("recuerda ingresar solo numeros y no dejar espacios vacios")
+            return false;
+        }
+    }
+    for(var i=0;i<email.length;i++){
+        if (!esEmail.test(email[i].value)){
+            alert("recuerda ingresar solo un email y no dejar el espacio vacio")
+            return false;
+        }
+    }
+    return true;
+}
+function registro(formulario){
+    if(validar(formulario)){
+        formulario.action = "login.html"
+    }        
+}
+function login(formulario){
+    if(validar(formulario)){
+        formulario.action = ""
+    }
+}
